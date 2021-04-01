5de25a5fb8be8847e250e8ef00293576ef3aaabd
I Gusti Ayu Ratu Putri Maharani <ayuratuputrimaha@gmail.com>
2021-04-01 20:05:00 +0800

    soal 1

---
+++ I0320049_soal 1.py
@@ -0,0 +1,7 @@
+nama = str(input("Masukkan nama pengunjung: "))
+jenis_kelamin = str(input("Masukkan jenis kelamin pengunjung: "))
+if jenis_kelamin == "l":
+    print("Selamat", "datang", "Tuan", nama+ "!")
+else:
+    print("Selamat", "datang", "Nyonya", nama+ "!" )
+print()
